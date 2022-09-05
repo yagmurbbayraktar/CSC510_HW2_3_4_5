8e8202815907588c67c4c7e2a541b1f954d4fec1
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-05 19:49:39 -0400

    Add files via upload

---
+++ test/test_bignum.py
@@ -0,0 +1,23 @@
+import math
+import random, getopt
+import os, sys, time, random
+import argparse
+import csv
+import math
+from xmlrpc.client import MAXINT
+from main import the
+from Num import Num
+import unittest
+
+class TestingBignum(unittest.TestCase):
+    def test_num(self):
+        num = Num()
+        for i in range(100):
+            num.add(i)
+        mid, div = num.mid(), num.div()
+        print("div", div)
+        print("mid", mid)
+        assert 50 <= mid and mid <= 52 and 30.5 < div and div< 32
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
---
+++ test/test_num.py
@@ -0,0 +1,22 @@
+import math
+import random, getopt
+import os, sys, time, random
+import argparse
+import csv
+import math
+from xmlrpc.client import MAXINT
+from main import the
+from Num import Num
+import unittest
+
+class TestingNum(unittest.TestCase):
+    def test_bignum(self):
+        num = Num()
+        the["nums"] = 32
+        for i in range(1000):
+            num.add(i)
+        assert 32 == len(num.has)
+
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
