db3eb47fd8cc2ba1a5143314fc9dd9294ab5cefb
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-09-07 01:43:49 -0400

    Update sym_test.py

--- test/sym_test.py
+++ test/sym_test.py
@@ -6,18 +6,15 @@ except ImportError as e:
 
 import sym
 
-def test_the():
-    pass
-
 def test_sym(sym_obj):
     for item in ["a", "a", "a", "a", "b", "b", "c"]:
         sym_obj.add(item)
     mode, entropy = sym_obj.mid(), sym_obj.div()
     entropy = (1000*entropy)//(1/1000)
-    print('mid:', mid, ' div:', entropy)
+    print('mid:', mid, 'div:', entropy)
     return mode=="a" and 1.37<=entropy and entropy<=1.38
 
 
-if "__main__"==__name__:
+if __name__=='__main__':
     sym_obj = sym.Sym()
     print(test_sym(sym_obj))
