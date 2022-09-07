8d219fe60f5f81a2d81befe6aa91edc701db8d78
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-09-07 01:33:54 -0400

    Update sym_test.py

--- test/sym_test.py
+++ test/sym_test.py
@@ -13,8 +13,8 @@ def test_sym(sym_obj):
     for item in ["a", "a", "a", "a", "b", "b", "c"]:
         sym_obj.add(item)
     mode, entropy = sym_obj.mid(), sym_obj.div()
-    # entropy = (1000*entropy)//(1/1000)
-    # oo({mid:mode, div:entropy})
+    entropy = (1000*entropy)//(1/1000)
+    print('mid:', mid, ' div:', entropy)
     return mode=="a" and 1.37<=entropy and entropy<=1.38
 
 
