24cfd97996e0bdb70011963d26517644fa005af0
yagmurbbayraktar <yagmurbasakbayraktar@gmail.com>
2022-09-13 00:14:26 -0400

    Update on data.py

--- src/data.py
+++ src/data.py
@@ -1,15 +1,15 @@
-from tokenize import String
-
+from csv import reader
+import re
 
 class data:
     def __init__(self, src):
         self.cols = None #summaries of data
         self.rows = {} #kept data
-        if type(src == String):
+        if type(src == str):
             pass
 
 
-someStringCHANGE = ""
+someStringCHANGE = "" #will come from another part of code.
 
 # Call `fun` on each row. Row cells are divided in `the.seperator`.
 def csv(fname, fun):
@@ -19,13 +19,18 @@ def csv(fname, fun):
     except:
         import os
         f = open(str(os.path.dirname(__file__)) + "/../data/" +str(fname))
-    
-    
-    text = f.read()
-    print((text))
+    csv_reader = reader(f)
+    for row in csv_reader:
+        t = []
+        for s1 in re.findall(row, sep):
+            t.append(s1) #coerce function will come here i dont have it yet. CHANGE
+        fun(t)
 
 
-csv("auto93.csv", None)
+
+
+
+
 
 
 
