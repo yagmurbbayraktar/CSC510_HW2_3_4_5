7ef771c8446db423f339ed973eb5f8bd89bd4f0e
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-09-12 23:33:21 -0400

    Update test_sym.py

--- test/test_sym.py
+++ test/test_sym.py
@@ -1,20 +1,25 @@
-try:
-    import sym
-except ImportError as e:
-    import sys
-    sys.path.append("../src")
+import math
+import random, getopt
+import os, sys, time, random
+import argparse
+import csv
+import math
+from xmlrpc.client import MAXINT
+import sys
+sys.path.append( '.' )
+from src.main import the
+from src.sym import Sym
+import unittest
 
-import sym
+class TestingSym(unittest.TestCase):
+    def test_sym(self):
+        sym = Sym()
+        for item in ["a", "a", "a", "a", "b", "b", "c"]:
+            sym.add(item)
+        mode, entropy = sym.mid(), sym.div()
+        entropy = (1000*entropy)//(1/1000)
+        print('mid:', mode, 'div:', entropy)
+        assert mode=="a" and 1.37<=entropy and entropy<=1.38
 
-def test_sym(sym_obj):
-    for item in ["a", "a", "a", "a", "b", "b", "c"]:
-        sym_obj.add(item)
-    mode, entropy = sym_obj.mid(), sym_obj.div()
-    entropy = (1000*entropy)//(1/1000)
-    print('mid:', mode, 'div:', entropy)
-    return mode=="a" and 1.37<=entropy and entropy<=1.38
-
-
-if __name__=='__main__':
-    sym_obj = sym.Sym()
-    print(test_sym(sym_obj))
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
