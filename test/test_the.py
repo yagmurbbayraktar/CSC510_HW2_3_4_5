95c512ed3cf8128a550ea0c606e06cf961cc334b
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-09-12 23:47:44 -0400

    Update test_the.py

--- test/test_the.py
+++ test/test_the.py
@@ -12,3 +12,24 @@ def test_the(argv):
 
 if __name__=='__main__':
     test_the(argv='this-is-first-argument')
+    
+import math
+import random, getopt
+import os, sys, time, random
+import argparse
+import csv
+import math
+from xmlrpc.client import MAXINT
+import sys
+sys.path.append( '.' )
+from src.main import the
+import unittest
+
+class TestingSym(unittest.TestCase):
+    def test_the(self):
+        sym = the()
+        
+        assert mode=="a" and 1.37<=entropy and entropy<=1.38
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
