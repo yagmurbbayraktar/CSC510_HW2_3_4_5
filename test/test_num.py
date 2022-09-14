0d555651ec475483d71dfa7b39281ed216a790a1
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-14 16:52:14 -0400

    Update test_num.py

--- test/test_num.py
+++ test/test_num.py
@@ -1,16 +1,16 @@
+
 s = __file__
-s = s[0:len(s)-20]
+s = s[0:len(s)-16]
 s = s+"/src"
 import sys
 sys.path.insert(1, s)
 from num import Num
 from main import the
 
-def test_num():
+def test_bignum():
         num = Num()
-        for i in range(100):
+        the["nums"] = 32
+        for i in range(1000):
             num.add(i)
-        mid, div = num.mid(), num.div()
-        print("div", div)
-        print("mid", mid)
-        return 0 if (50 <= mid and mid <= 52 and 30.5 < div and div< 32) else 1
+        return 0 if(32 == len(num.has)) else 1
+
