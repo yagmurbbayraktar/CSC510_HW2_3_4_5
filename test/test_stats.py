d3c21010ce1aaa679786ca67bf3ea73717efb71b
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-14 16:59:53 -0400

    Update test_stats.py

--- test/test_stats.py
+++ test/test_stats.py
@@ -17,15 +17,15 @@ import unittest
 
 class Testingstats(unittest.TestCase):
     def test_stats(self):
-        data = data("../data/auto93.csv")
+        data1 = data("../data/auto93.csv")
         def div(col):
             return col.div()
         def mid(col):
             return col.mid()
-        print("xmid", o(data.stats(2, data.cols.x, mid)))
-        print("xdiv", o(data.stats(3, data.cols.x, div)))
-        print("ymid", o(data.stats(2, data.cols.y, mid)))
-        print("ydiv", o(data.stats(3, data.cols.y, div)))
+        print("xmid", o(data1.stats(2, data1.cols.x, mid)))
+        print("xdiv", o(data1.stats(3, data1.cols.x, div)))
+        print("ymid", o(data1.stats(2, data1.cols.y, mid)))
+        print("ydiv", o(data1.stats(3, data1.cols.y, div)))
         assert True
 
 
