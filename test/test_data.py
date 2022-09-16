dc52bb0dc44d47101923cca0e09c89731c72ce76
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-16 09:49:32 -0400

    Update test_data.py

--- test/test_data.py
+++ test/test_data.py
@@ -1,11 +1,6 @@
-
-s = __file__
-s = s[0:len(s)-16]
-s = s+"/src"
 import sys
-sys.path.insert(1, s)
-from main import the,oo
-from data import data
+from src.main import the,oo
+from src.data import data
 import math
 import random, getopt
 import os, sys, time, random
@@ -14,7 +9,7 @@ import csv
 import math
 
 def test_data():
-    data1 = data("../data/data1.csv")
+    data1 = data("/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5/data/auto93.csv")
     for _, col in enumerate(data1.cols.y):
         oo(col)
     return 0 if len(data1.cols.y) == 3 else 1
