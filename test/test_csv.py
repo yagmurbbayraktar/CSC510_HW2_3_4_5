dd2c9135b49a06a209f31388ff57696918d183c6
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-09-19 18:08:36 -0400

    Update test_csv.py

--- test/test_csv.py
+++ test/test_csv.py
@@ -7,18 +7,17 @@ import math
 from xmlrpc.client import MAXINT
 import sys
 sys.path.append( '.' )
-from src.main import the, oo
+from src.main import the, oo, csv
 from src.row import Row
-from src.data import csv
 import unittest
 
 class TestingCSV(unittest.TestCase):
     def test_csv(self):
         n = 0
+        data = csv("/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5/data/auto93.csv", Row)
         while n < 10:
-            csv("/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5/data/auto93.csv", Row)
+            assert oo(Row(n) == print(Row(n)))
             n += 1
-            assert oo(Row) == print(Row)
         assert True
 
 if __name__ == "__main__":
