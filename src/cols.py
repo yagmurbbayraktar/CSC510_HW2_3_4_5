5c4e78908b5980d72dbc163232e72e93269ef8d9
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-20 03:59:48 -0400

    Update cols.py

--- src/cols.py
+++ src/cols.py
@@ -10,12 +10,12 @@ class Cols:
         self.y = []
         self.klass = None
     
-        for idx, name in enumerate(names): 
-                    col = name[0].isupper() and Num(idx, name) or Sym(idx, name)  
-                    self.all.append(col)
-                    if name[-1] != ':':
-                        self.y.append(col) if name[-1] == '-' or name[-1] == '+' else self.x.append(col)
-                        if name[-1] == '!':
+        for c, s in enumerate(names): 
+                    col = s[0].isupper() and Num(c, s) or Sym(c, s)  
+                    push(self.all,col)
+                    if s[-1] != ':':
+                        push(self.y,col) if s[-1] == '-' or s[-1] == '+' else push(self.x,col)
+                        if s[-1] == '!':
                             self.klass = col
 
 
