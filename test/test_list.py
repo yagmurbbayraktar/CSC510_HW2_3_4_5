310882d1f0a6d7efe3b52d9538b8d81df269ac1f
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-20 00:00:45 -0400

    Update test_list.py

--- test/test_list.py
+++ test/test_list.py
@@ -11,8 +11,7 @@ from src.main import the, oo, csv
 from src.row import Row
 import unittest
 
-class TestingLIST(unittest.TestCase):
-    def test_list(self):
+def test_list(self):
         t = {}
         test_names = {'test_ls':1, 'test_bad':1, 'test_all':1, 'test_bigum': 1, 'test_csv': 1, 'test_data': 1, 
         'test_num': 1, 'test_stats': 1, 'test_sym': 1, 'test_the': 1
@@ -22,7 +21,3 @@ class TestingLIST(unittest.TestCase):
         t = [(i, v) for i, v in enumerate(sorted(t.values()))]
         t = dict(t)
         return t
-
-if __name__ == "__main__":
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
-
