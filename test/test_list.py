ff8170b41c77416e85baacfd841b37eca0d46684
Nisanchhetri <46899689+Nisanchhetri@users.noreply.github.com>
2022-09-19 23:56:50 -0400

    Update test_list.py

--- test/test_list.py
+++ test/test_list.py
@@ -1,3 +1,28 @@
-def LIST():
-    my_dict = {1: 'test1', 2: 'test2', 3: 'test3', 4: 'test4'}
-    return my_dict
+import math
+import random, getopt
+import os, sys, time, random
+import argparse
+import csv
+import math
+from xmlrpc.client import MAXINT
+import sys
+sys.path.append( '.' )
+from src.main import the, oo, csv
+from src.row import Row
+import unittest
+
+class TestingLIST(unittest.TestCase):
+    def test_list(self):
+        t = {}
+        test_names = {'test_ls':1, 'test_bad':1, 'test_all':1, 'test_bigum': 1, 'test_csv': 1, 'test_data': 1, 
+        'test_num': 1, 'test_stats': 1, 'test_sym': 1, 'test_the': 1
+        }
+        for i, (k, _) in enumerate(test_names.items()):
+            t[i] = k
+        t = [(i, v) for i, v in enumerate(sorted(t.values()))]
+        t = dict(t)
+        return t
+
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+
