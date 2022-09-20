3b73c8578d4c519c8da390b0d5f35bb1d95d760d
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-19 23:57:37 -0400

    Update test_sym.py

--- test/test_sym.py
+++ test/test_sym.py
@@ -6,20 +6,16 @@ import csv
 import math
 from xmlrpc.client import MAXINT
 import sys
-sys.path.append( '.' )
+sys.path.insert(0, '/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5')
 from src.main import the
 from src.sym import Sym
 import unittest
 
-class TestingSym(unittest.TestCase):
-    def test_sym(self):
+def test_sym():
         sym = Sym()
         for item in ["a", "a", "a", "a", "b", "b", "c"]:
             sym.add(item)
         mode, entropy = sym.mid(), sym.div()
         entropy = (1000*entropy)//1/1000
         print('mid:', mode, 'div:', entropy)
-        assert mode=="a" and 1.37<=entropy and entropy<=1.38
-
-if __name__ == "__main__":
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+        return 0 if( mode=="a" and 1.37<=entropy and entropy<=1.38) else 1
