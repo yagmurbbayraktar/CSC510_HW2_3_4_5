9dc3933131a9f54f70fde4435ef0d376ccf972ab
priyamshah0709 <53808524+priyamshah0709@users.noreply.github.com>
2022-09-20 09:43:05 -0400

    Update test_ls.py

--- test/test_ls.py
+++ test/test_ls.py
@@ -2,7 +2,6 @@ from test_list import test_list
 
 def test_LS():
     my_list = test_list()
-    print (test_list())
     print ("Examples lua csv -e ...")
     for Key, value in my_list.items():
         print(value)
