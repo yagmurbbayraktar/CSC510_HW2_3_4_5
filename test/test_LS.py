d11fda750d218f8281a1589a55a0128dd9ab1068
priyam <priyamshah0709@gmail.com>
2022-09-19 22:02:24 -0400

    Updated LS()

---
+++ test/test_LS.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from typing import List
+from test_LIST import LIST
+
+def test_LS():
+    my_list = LIST()
+    print (LIST())
+    print ("Examples lua csv -e ...")
+    for Key, value in my_list.items():
+        print(value)
+    return True
+test_LS()
