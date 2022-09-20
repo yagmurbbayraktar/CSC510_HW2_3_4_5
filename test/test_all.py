b0342f8aae4fa5070ec9d93fd514af740794d6c4
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-20 00:22:02 -0400

    Update test_all.py

--- test/test_all.py
+++ test/test_all.py
@@ -2,7 +2,6 @@
 from test_bad import test_bad
 from test_bignum import test_bignum
 from test_data import test_data
-from test_ls import test_LS
 from test_num import test_num
 from test_sym import test_sym
 from test_the import test_the
@@ -15,7 +14,26 @@ def main():
     print("-----------------------------------")
     print("!!!!!!	FAIL	    LIST     true\n")
     print("-----------------------------------\n")
-    output = test_bad()+test_bignum()+test_csv()+test_data()+test_LS()+test_num()+test_sym()+test_the()+test_stats()+test_csv()
+    print("""Examples lua csv -e ...
+	ALL
+	BAD
+	LIST
+	LS
+	bignum
+	csv
+	data
+	num
+	stats
+	sym
+	the""")
+    print("!!!!!!	PASS	     LS	     true\n")
+    print("-----------------------------------")
+    output = test_bad()
+    output = +test_bignum()
+    print("!!!!!!	PASS	     bignum	 true\n")
+    output = +test_csv()
+    print("!!!!!!	PASS	     csv	 true\n")
+    output = +test_data()+test_num()+test_sym()+test_the()+test_stats()+test_csv()
     print(output, " tests failed")
     print(7 - output, " tests passed")
     return 0
