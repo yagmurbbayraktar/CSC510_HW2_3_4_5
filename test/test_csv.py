54d811acbf24e3908f92d944decd8a9db1051346
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-20 01:09:11 -0400

    Update test_csv.py

--- test/test_csv.py
+++ test/test_csv.py
@@ -10,14 +10,13 @@ sys.path.append( '.' )
 from src.main import the, oo, csv
 from src.row import Row
 import unittest
-n = 0
 def test_csv():
-        def row(input):
-                global n
+        n = 0
+        def row(n, input):
                 n+=1
                 if n >10:
                         return
                 else:
                         print(input)
-        data = csv("/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5/data/auto93.csv", row)
+        data = csv("/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5/data/auto93.csv", row,n)
         return 0
