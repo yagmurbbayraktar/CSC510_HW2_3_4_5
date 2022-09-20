833103569dab460e43c74601d6ebbf6a945079f7
Zhihao Wang <80023278+wangz35@users.noreply.github.com>
2022-09-20 01:56:24 -0400

    Update test_bignum.py

--- test/test_bignum.py
+++ test/test_bignum.py
@@ -2,9 +2,10 @@ import sys
 import sys
 sys.path.insert(0, '/home/runner/work/CSC510_HW2_3_4_5/CSC510_HW2_3_4_5/src')
 from num import Num
-from main import the
+from main import the,cli
 
 def test_bignum():
+        cli(False,'auto93.csv', 512,10019,",]]")
         num = Num()
         the["nums"] = 32
         for i in range(1000):
